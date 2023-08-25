c550223335e754279ef8703a57cbb86e31d35ded
INTRON-26 <pvdheerajreddy@gmail.com>
2023-08-25 17:18:20 +0530

    commit 2

--- helloworld/src/App.js
+++ helloworld/src/App.js
@@ -4,6 +4,8 @@ import Greet from './Components/Greet'
 import Welcome from './Components/Welcome'
 import Hello from './Components/Hello';
 import Hey from './Components/Hey';
+import Message from './Components/message';
+import Counter from './Components/Counter';
 
 
 // import greet2 from './Greet'
@@ -15,6 +17,8 @@ function App() {
       <Hello name = "Dheeraj"/>
       <Hello name = "Intron"/>
       <Hey/>
+      <Message/>
+      <Counter/>
     </div>
   );
 }
